07ab627aa366d38841c2dc6e5634243aa8cbacb6
TakuoIshida <archersparadox.100@gmail.com>
2020-04-21 14:58:24 +0900

    initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import {} from '.components';
+
+import styles from './App.module.css';
+
+class App extends React.Component {
+
+
+    render(){
+        return (
+
+        )
+    }
+}
+
+export default App;

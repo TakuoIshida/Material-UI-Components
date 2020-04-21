acd31fa582ae78585bb6bf69c5ba26d33dbe5355
TakuoIshida <archersparadox.100@gmail.com>
2020-04-22 00:40:52 +0900

    margin +

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import Button from './components/Button/Button';
 
+import styles from './App.module.css';
+
 class App extends Component {
 
     render(){
         return (
-            <div className="container">
+            <div>
                 <Button />
             </div>
         )
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -7,15 +7,17 @@ import styles from './Button.module.css';
 class Buttons extends Component {
     render() {
         return (
-            <div  className={ styles.container }>
-                <CardActions>
-                    <Button >Default</Button>
-                    <Button variant="contained" color="primary">Primary</Button>
-                    <Button variant="outlined" color="secondary"> Secondary</Button>               
-                    <Button variant="contained" disabled>Disabled</Button>
-                    <Button variant="contained" size="small" color="primary" href="#contained-buttons">small Link</Button>
-                    <Button variant="contained" size="large" color="primary" href="#contained-buttons">large Link</Button>
-                </CardActions>
+            <div className={ styles.container }>
+                <div>
+                    <Button className={styles.margin}>Default</Button>
+                    <Button variant="contained" color="primary"  className={styles.margin}>Primary</Button>
+                    <Button variant="outlined" color="secondary"  className={styles.margin}> Secondary</Button>
+                </div>
+                <div>
+                    <Button variant="contained" disabled className={styles.margin}>Disabled</Button>
+                    <Button variant="contained" size="small" color="primary" href="#contained-buttons" className={styles.margin}>small Link</Button>
+                    <Button variant="contained" size="large" color="primary" href="#contained-buttons" className={styles.margin}>large Link</Button>
+                </div>
             </div>
         )
     }

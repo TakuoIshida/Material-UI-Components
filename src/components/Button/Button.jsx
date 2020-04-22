a69596f9f5c5f52605b83fb7c19fe6ce77a89909
TakuoIshida <archersparadox.100@gmail.com>
2020-04-22 10:07:26 +0900

    button with Icons

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,10 @@
 import React, { Component }  from 'react';
 import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
+import Icon from '@material-ui/core/Icon';
+import SaveIcon from '@material-ui/icons/Save';
 
 import styles from './Button.module.css';
 
@@ -16,6 +21,12 @@ class Buttons extends Component {
                     <Button variant="contained" disabled className={styles.margin}>Disabled</Button>
                     <Button variant="contained" size="small" color="primary" href="#contained-buttons" className={styles.margin}>small Link</Button>
                     <Button variant="contained" size="large" color="primary" href="#contained-buttons" className={styles.margin}>large Link</Button>
+                </div>
+                <div>
+                    <Button variant="contained" color="secondary" className={styles.margin} startIcon={<DeleteIcon/>}>Delete</Button>
+                    <Button variant="contained" color="default" className={styles.margin} startIcon={<CloudUploadIcon />}>Upload</Button>
+                    <Button variant="contained" color="primary" className={styles.margin} startIcon={<KeyboardVoiceIcon />}>Talk</Button>
+                    <Button variant="contained" color="primary" className={styles.margin} startIcon={<SaveIcon />}>Save</Button>
                 </div>
             </div>
         )

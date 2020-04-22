72a57cb584b1d99cc399e1aa7e21a50ca1861518
TakuoIshida <archersparadox.100@gmail.com>
2020-04-22 18:31:09 +0900

    AppBar作成

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Button from './components/Button/Button';
+import AppBars from './components/AppBar/AppBar';
 
 import styles from './App.module.css';
 
@@ -8,7 +9,9 @@ class App extends Component {
     render(){
         return (
             <div>
+                <AppBars />
                 <Button />
+                <AppBars />
             </div>
         )
     }
---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import styles from './AppBar.module.css';
+
+
+class AppBars extends Component {
+    render() {
+        return(
+            <div className={styles.root}>
+                <AppBar position="sticky">
+                    <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu" className={styles.menuButton}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={styles.title}>
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    }
+};
+
+export default AppBars;
